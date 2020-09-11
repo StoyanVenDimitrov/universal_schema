76a78c73945c5f0ee462612e53177fcfab10b910
Stoyan Dimitrov <stoyan.dimitrov@dtms.de>
2020-09-11 09:28:03 +0200

    reading from .pdf to .txt

--- preprocessing/prepare_train_input.py
+++ preprocessing/prepare_train_input.py
@@ -1,9 +1,19 @@
 import nltk
 import numpy as np
+from itertools import groupby
 
 def run(file_path):
+    # reading from a .pdf converted to .txt:
+    content_as_list = []
     with open(file_path) as file:
-        content = [nltk.word_tokenize(x) for x in file.readlines()] # TODO: too slow, replace with split and strip
+        for line in file.readlines():
+            line_as_list = nltk.word_tokenize(line) # good tokenizer will avoid splitting on each . 
+            if len(line_as_list) >3:
+                content_as_list = content_as_list + line_as_list
+    i = (list(g) for _, g in groupby(content_as_list, key='.'.__ne__))
+    content = [a + b for a, b in zip(i, i)]
+    print(content)
+
     entity_index = ('data', 'results')#, 'introduction', 'evaluation')
     for sentence in content:
         found_here = []
@@ -15,7 +25,7 @@ def run(file_path):
             for i,w in enumerate(sentence):
                 if w in common_entites:
                     if last_found != None and last_found != w:
-                        found_here.append((last_found, w))
+                        found_here.append((last_found, w))  
                         found_here_on.append((last_found_on, i))
                     last_found_on = i
                     last_found = w
@@ -23,6 +33,7 @@ def run(file_path):
         #     [ (e[0], e[1], sentence[i[0]+1:i[1]]) for e, i in zip(found_here, found_here_on)]
         #     )
         result = [ (e[0], e[1], '$ARG1 '+' '.join(sentence[i[0]+1:i[1]])+' $ARG2') for e, i in zip(found_here, found_here_on)]
+
 
 if __name__ == "__main__":
     run("data/texts.txt")
