95e6af3f320f50ee997bdc3df2c2da04728ece02
Stoyan Dimitrov <stoyan.dimitrov@dtms.de>
2020-09-12 14:05:20 +0200

    extraction seems to work on larger data

--- preprocessing/prepare_train_input.py
+++ preprocessing/prepare_train_input.py
@@ -14,12 +14,13 @@ def read_from_pdf(file_path):
     return content
 
 def read_index(index_path):
-    entity_index = []
+    entity_index = dict()
     with open(index_path) as file:
         for line in file.readlines():
             line_list = line.strip().split(',')
             if len(line_list) > 1 and not line_list[0].isdigit():
-                entity_index.append(line_list[0].lower())
+                name = line_list[0].lower()
+                entity_index[name.upper()] = name.split()
     # TODO: Map entites with 'see ...' together
     return entity_index
 
@@ -32,7 +33,7 @@ def run(file_path, index_path):
 
     for sentence in content:
 
-        entity_index = {'DL':['deep', 'learning'], 'DEEP':['deep'], 'FORM':['formalisms'], 'GUIDE':['guide']}
+        #entity_index = {'DL':['0-1', 'loss'], 'DEEP':['deep'], 'FORM':['formalisms'], 'GUIDE':['guide']}
         sentence_iter = iter(enumerate(sentence))
 
         found_here = []
@@ -46,27 +47,27 @@ def run(file_path, index_path):
             if not candidates:
                 continue
             # if we match single-word index token directly
-            if len(candidates)==1:
-                w = candidates[0][0]
-                span = (i, i+1)
-            else:
-                str_the_rest = ' '.join(sentence[i:]).lower()
-                to_remove = []
-                for c in candidates:
-                    str_c = ' '.join(c[1])
-                    seen_here = str_the_rest.find(str_c, 0, len(str_c))
-                    to_remove.append(seen_here)
-                candidates = [candidate for seen, candidate in zip(to_remove, candidates) if seen>-1]
-                single_candidate = max(candidates, key=lambda item: len(item[1]))
-                # iter only if something was found:
-                start_pos = i
+
+            str_the_rest = ' '.join(sentence[i:]).lower()
+            to_remove = []
+            for c in candidates:
+                str_c = ' '.join(c[1])
+                seen_here = str_the_rest.find(str_c, 0, len(str_c))
+                to_remove.append(seen_here)
+            candidates = [candidate for seen, candidate in zip(to_remove, candidates) if seen>-1]
+            if not candidates:
+                continue
+            single_candidate = max(candidates, key=lambda item: len(item[1]))
+            # iter only if something was found:
+            start_pos = i
+            end_pos = i +1
+            for skip in range(len(single_candidate[1])-1):
+                i, _ = next(sentence_iter)
                 end_pos = i +1
-                for skip in range(len(single_candidate[1])-1):
-                    i, _ = next(sentence_iter)
-                    end_pos = i +1
-                span = (start_pos, end_pos)
-                w = single_candidate[0]
+            span = (start_pos, end_pos)
+            w = single_candidate[0]
             if last_found != None and last_found != w:
+                # apply aliases from list of aliases
                 found_here.append((last_found, w))  
                 found_here_on.append((last_found_on, span[0]))
             last_found_on = span[1]
@@ -74,10 +75,10 @@ def run(file_path, index_path):
 
         result = [ (e[0], e[1], '$ARG1 '+' '.join(sentence[i[0]:i[1]])+' $ARG2') for e, i in zip(found_here, found_here_on) if i[0]!=i[1]]
         text_examples.update(result)
-
+    print(len(text_examples))
     return text_examples
 
 if __name__ == "__main__":
-    # run("data/[11]part-2-chapter-6.txt", "data/[28]index.txt")
-    run("data/texts.txt", "data/[28]index.txt")
+    run("data/[11]part-2-chapter-6.txt", "data/[28]index.txt")
+    # run("data/texts.txt", "data/[28]index.txt")
 
